242449a96a3796be73a56591ef08e2bb6b02963a
Anton Krackov <antonkrachkov@hollandandbarrett.com>
2024-10-20 03:38:07 +0400

    fix: try to fix sha key

--- generate-credentials.js
+++ generate-credentials.js
@@ -6,7 +6,7 @@ const credentials = {
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
   private_key: `
-${process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')}
+${process.env.GOOGLE_PRIVATE_KEY}
 `,
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID,
